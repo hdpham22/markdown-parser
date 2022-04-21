16d1b1888e220a17fa90fe8d459d291ac08fe87b
hdpham22 <103155924+hdpham22@users.noreply.github.com>
2022-04-21 11:04:40 -0700

    Working tester

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,11 @@
 import static org.junit.Assert.*;
 
 import java.beans.Transient;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.*;
 
@@ -11,7 +16,12 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testGetLinks() {
-        
+    public void testGetLinks() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        List<String> expected = List.of("https://something.com", "some-thing.html");
+
+        assertEquals(expected, links);
     }
 }
