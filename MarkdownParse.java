b5673ad077e4b13c8ade3faabdf43e75704561bc
hdpham22 <103155924+hdpham22@users.noreply.github.com>
2022-04-28 10:31:48 -0700

    mistake

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -22,7 +22,6 @@ public class MarkdownParse {
             if(openBracket == -1) {break;}
             else {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
             }
 
         }
